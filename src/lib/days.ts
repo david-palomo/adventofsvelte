5ee208dc2da611eb45f0d6f3d288c2d45fdfd260
code406 <davidpm99@gmail.com>
2023-12-29 02:11:36 +0100

    feat: days data & days-grid layout. using phosphor icons

---
+++ src/lib/days.ts
@@ -0,0 +1,28 @@
+type Day = { day: number; completed: boolean; title: string };
+
+export const days: Day[] = [
+	{ day: 1, completed: true, title: 'Naughty or Nice' },
+	{ day: 2, completed: false, title: 'Merry Munch-o-Meter' },
+	{ day: 3, completed: false, title: 'Jingle Bell Balancer' },
+	{ day: 4, completed: false, title: 'Heart of Christmas' },
+	{ day: 5, completed: false, title: 'Present Progress' },
+	{ day: 6, completed: false, title: 'Misteltoe Metronome' },
+	{ day: 7, completed: false, title: 'Morse Mischief' },
+	{ day: 8, completed: false, title: "Santa's Mysterious Deck of Doubles" },
+	{ day: 9, completed: false, title: "Santa's Final Countdown" },
+	{ day: 10, completed: false, title: 'Pop-up! Spreading the holiday cheer' },
+	{ day: 11, completed: false, title: 'Tinsel Transformers' },
+	{ day: 12, completed: false, title: 'Greetings and salutations' },
+	{ day: 13, completed: false, title: 'Jingle Bell Balancer 2.0' },
+	{ day: 14, completed: false, title: 'Snowfall Symphony' },
+	{ day: 15, completed: false, title: 'Sparkling Snowglobes' },
+	{ day: 16, completed: false, title: 'Lessons from Amazon' },
+	{ day: 17, completed: false, title: 'Festive Funds' },
+	{ day: 18, completed: false, title: "Santa's Quiz" },
+	{ day: 19, completed: false, title: 'Polar Lights' },
+	{ day: 20, completed: false, title: 'Jingle Jokes' },
+	{ day: 21, completed: false, title: 'Oh the weather outside is frightful' },
+	{ day: 22, completed: false, title: 'Snowflake Silhouettes' },
+	{ day: 23, completed: false, title: 'ElfNet Social' },
+	{ day: 24, completed: false, title: "Santa's Magical Tracker" }
+];
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,55 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	import { days } from '$lib/days';
+	import { CalendarBlank as Calendar, CheckCircle, CircleDashed } from 'phosphor-svelte';
+	// TODO: (WHEN_LUCIDE_FIXED) Change import to lucide-svelte. Import Calendar, CheckCircle, CircleDashed
+</script>
+
+<h2 class="mt-1 mb-7 md:mb-10 flex items-center justify-center space-x-3">
+	<Calendar class="w-6 h-6 md:w-7 md:h-7 text-primary" />
+	<span class="text-xl md:text-2xl font-bold pb-0.5">Calendar</span>
+</h2>
+
+<div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4">
+	{#each days as day}
+		{#if day.completed}
+			<a href="day/{day.day}">
+				<div
+					class="day-card completed border-2 rounded-md px-4 py-3
+					border-[--pico-border-color] hover:border-primary"
+				>
+					<div class="flex items-center space-x-1 justify-between">
+						<span class="font-bold">Day {day.day}</span>
+						<CheckCircle class="text-primary w-6 h-6" />
+						<!-- TODO: (WHEN_LUCIDE_FIXED) Remove w/h -->
+					</div>
+					<span class="text-sm">{day.title}</span>
+				</div>
+			</a>
+		{:else}
+			<div
+				class="day-card incomplete border-2 rounded-md px-4 py-3 border-dashed
+					border-[--pico-border-color] text-[--pico-border-color]"
+			>
+				<div class="flex items-center space-x-1 justify-between">
+					<span class="font-bold">Day {day.day}</span>
+					<CircleDashed class="w-6 h-6" />
+					<!-- TODO: (WHEN_LUCIDE_FIXED) Remove w/h -->
+				</div>
+				<span class="text-sm">{day.title}</span>
+			</div>
+		{/if}
+	{/each}
+</div>
+
+<style>
+	.day-card {
+		transition-timing-function: cubic-bezier(0.4, 0, 0.2, 1);
+		transition-duration: 150ms;
+	}
+	.day-card.completed {
+		transition-property: border;
+	}
+	.day-card.incomplete {
+		transition-property: border, color;
+	}
+</style>
